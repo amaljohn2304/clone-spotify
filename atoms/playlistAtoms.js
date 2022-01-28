a45964d5bb2d881de0c048f966fc2b8e54211f4a
amaljohn2304 <73713382+amaljohn2304@users.noreply.github.com>
2022-01-28 10:49:57 +0530

    initial

---
+++ atoms/playlistAtoms.js
@@ -0,0 +1,11 @@
+import { atom } from 'recoil';
+
+export const playlistState = atom({
+    key: 'playlistState',
+    default: null,
+});
+
+export const playlistIdState = atom({
+    key: 'playlistIdState',
+    default: '502apEZig2j9c7mpl8mTEg',
+});
